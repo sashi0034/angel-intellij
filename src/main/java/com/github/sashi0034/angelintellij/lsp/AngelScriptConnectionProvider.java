9e7908b1c9219999eb3ad34a80f33ec443ccf6ad
sashi <satoshi3104.nishi@gmail.com>
2025-02-24 09:08:20 +0900

    Embed a server file

--- src/main/java/com/github/sashi0034/angelintellij/lsp/AngelScriptConnectionProvider.java
+++ src/main/java/com/github/sashi0034/angelintellij/lsp/AngelScriptConnectionProvider.java
@@ -2,14 +2,40 @@ package com.github.sashi0034.angelintellij.lsp;
 
 import com.intellij.execution.configurations.GeneralCommandLine;
 import com.redhat.devtools.lsp4ij.server.OSProcessStreamConnectionProvider;
+import org.apache.commons.compress.utils.IOUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class AngelScriptConnectionProvider extends OSProcessStreamConnectionProvider {
-
     public AngelScriptConnectionProvider() {
-        GeneralCommandLine commandLine = new GeneralCommandLine(
-                "node",
-                "C:\\dev\\lang\\angel-lsp\\server\\out\\server.js",
-                "--stdio"); // FIXME
-        super.setCommandLine(commandLine);
+        // TODO: npx esbuild src/server.ts --bundle --outfile=server-bundle.js --minify --platform=node のスクリプトを書く
+
+        InputStream input = getClass().getResourceAsStream("/js/server-bundle.js");
+        if (input == null) {
+            throw new IllegalStateException("Can't find js/server-bundle.js");
+        }
+
+        try {
+            // Create a temporary file for the js/server-bundle.js
+            File tempJsFile = File.createTempFile("as-server-bundle", ".js");
+            try (FileOutputStream fos = new FileOutputStream(tempJsFile)) {
+                IOUtils.copy(input, fos);
+            } catch (IOException e) {
+                throw new IllegalStateException("Can't create a temporary file for js/server-bundle.js", e);
+            }
+
+            // Build the command line
+            GeneralCommandLine commandLine = new GeneralCommandLine(
+                    "node",
+                    tempJsFile.getAbsolutePath(),
+                    "--stdio");
+
+            super.setCommandLine(commandLine);
+        } catch (IOException e) {
+            throw new IllegalStateException("Can't create a temporary file for js/server-bundle.js", e);
+        }
     }
 }
